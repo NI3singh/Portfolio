d61975811313ee1b394014b1862bf33cf14bfde8
NI3singh <itsnits333@gmail.com>
2025-09-03 16:41:36 +0530

    solving some errors-3

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import Experience from '@/components/Experience';
 import Contact from '@/components/Contact';
 import Footer from '@/components/Footer';
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 function App() {
   return (
